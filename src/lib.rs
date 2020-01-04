ec812c150611ba0de2e8ba20aaad1072f8341694
zmant <zmant724@aliyun.com>
2020-01-04 20:26:39 +0800

    Refactor the QueryParams library

--- src/lib.rs
+++ src/lib.rs
@@ -1,189 +1,38 @@
+//! A general library for url query string
+//!
+//! This crate is a general purpose library for common function found when working
+//! with the request's query parameters.
+//!
+//! This library aims to implement the [querystring](https://nodejs.org/docs/latest-v11.x/api/querystring.html) function in Nodejs
+//!
+//! ## Supported Types
+//! For the library user, urlqstring will supports struct, map, and the string of query parameters style
+//!
+//! ### Usage
+//! See the examples as below.
+//! ```rust
+//! use urlqstring::QueryParams;
+//! fn main() {
+//!     let enc1 = QueryParams::from(vec![
+//!         ("params", "度"),
+//!         ("enc", "=-)(&#+~·@…%^$:*-_."),
+//!     ]).stringify();
+//!     let res1: String= String::from("params=%E5%BA%A6&enc=%3D-)(%26%23%2B~%C2%B7%40%E2%80%A6%25%5E%24%3A*-_.&");
+//!     assert_eq!(enc1, res1);
+//! }
+//! ```
 
-type QueryParam<'a> = (&'a str, &'a str);
-type QueryParams<'a> = Vec<QueryParam<'a>>;
+mod querystring;
 
-pub mod querystring {
-    use crate::QueryParams;
-    use std::collections::HashMap;
-
-    pub trait QueryParamGet {
-        fn get_value(&self, key: &str) -> Option<&str>;
-        fn replace_key(&self, old_key: &str, new_key: &str) -> String;
-        fn replace_keys(&self, old_key: Vec<&str>, new_key: Vec<&str>) -> String;
-        fn replace_value(&self, old_value: &str, new_value: &str) -> String;
-        fn replace_values(&self, old_value: Vec<&str>, new_value: Vec<&str>) -> String;
-    }
-
-    impl QueryParamGet for &str {
-        fn get_value(&self, key: &str) -> Option<&str> {
-            let parameters: Vec<&str> = self.split('&').collect();
-            for value in parameters {
-                if value.contains('=') &&  value.contains(key){
-                    return Some(value.splitn(2, "=").collect::<Vec<&str>>()[1]);
-                } else if value.contains(key) {
-                    return Some("");
-                } else {
-                    continue;
-                }
-            }
-            None
-        }
-
-        fn replace_key(&self, old_key: &str, new_key: &str) -> String {
-            let k1 = "&".to_owned() + old_key;
-            if self.contains(&k1) {
-                let k2 = "&".to_owned() + new_key;
-                self.replacen(&k1, &k2, 1)
-            } else if self.starts_with(old_key){
-                self.replacen(old_key, new_key, 1)
-            } else {
-                String::from("")
-            }
-        }
-
-        fn replace_keys(&self, old_keys: Vec<&str>, new_keys: Vec<&str>) -> String {
-            assert_eq!(old_keys.len(), new_keys.len());
-            let mut res : String = String::from(*self);
-            for ( idx, old_key ) in old_keys.iter().enumerate() {
-                let k1 = "&".to_owned() + old_key;
-                if res.contains(&k1) {
-                    let k2 = "&".to_owned() + new_keys[idx];
-                    res = res.replacen(&k1, &k2, 1);
-                } else if res.starts_with(old_key) {
-                    res = res.replacen(old_key, new_keys[idx], 1);
-                }
-            }
-            res
-        }
-
-        fn replace_value(&self, old_value: &str, new_value: &str) -> String {
-            let k1 = "=".to_owned() + old_value + "&";
-            if self.contains(&k1) {
-                let k2 = "=".to_owned() + new_value + "&";
-                self.replacen(&k1, &k2, 1)
-            } else if self.ends_with(old_value) {
-                self.replacen(old_value, new_value, 1)
-            } else {
-                String::from("")
-            }
-        }
-
-        fn replace_values(&self, old_values: Vec<&str>, new_values: Vec<&str>) -> String {
-            assert_eq!(old_values.len(), new_values.len());
-            let mut res : String = String::from(*self);
-            for (idx, old_value) in old_values.iter().enumerate() {
-                let k1 = "=".to_owned() + old_value + "&";
-                if res.contains(&k1) {
-                    let k2 = "=".to_owned() + new_values[idx] + "&";
-                    res = res.replacen(&k1, &k2, 1);
-                } else if res.ends_with(old_value) {
-                    res = res.replacen(old_value, new_values[idx], 1);
-                }
-            }
-            res
-        }
-
-    }
-
-    pub fn stringify(query: QueryParams) -> String {
-        query.iter().fold(String::new(), |acc, x| {
-            acc + &escape(&x.0) + "=" + escape(&x.1).as_ref() + "&"
-        })
-    }
-
-    pub fn json(query: &str) -> String {
-        let parameters: Vec<&str> = query.split('&').collect();
-        let mut res: String = String::new();
-        for (idx, kvs) in parameters.iter().enumerate() {
-            let kv: Vec<&str>;
-            if kvs.contains('=') {
-                kv = kvs.splitn(2, "=").collect();
-            } else {
-                kv = vec![kvs, ""];
-            }
-            if idx == parameters.len() - 1 {
-                res.push_str(&format!(r#""{}":"{}""#, kv[0], kv[1]));
-            }
-            else {
-                res.push_str(&format!(r#""{}":"{}","#, kv[0], kv[1]));
-            }
-        }
-        format!("{{{}}}", res)
-    }
-
-    pub fn parse(query: &str) -> HashMap<&str,&str> {
-        let parameters: Vec<&str> = query.split('&').collect();
-        let mut res: HashMap<&str,&str> = HashMap::new();
-        for kvs in parameters {
-            let kv: Vec<&str>;
-            if kvs.contains('=') {
-                kv = kvs.splitn(2, "=").collect();
-            } else {
-                kv = vec![kvs, ""];
-            }
-            res.insert(kv[0], kv[1]);
-        }
-        res
-    }
-
-    fn escape(str: &str) -> String {
-        let mut enc = Vec::<u8>::new();
-        for ch in str.as_bytes() {
-            if keep_as(*ch) {
-                enc.push(*ch);
-            } else {
-                enc.push(0x25);
-                let n1 = (*ch >> 4) & 0xf;
-                let n2 = *ch & 0xf;
-                enc.push(to_dec_ascii(n1));
-                enc.push(to_dec_ascii(n2));
-            }
-        }
-        String::from_utf8(enc).unwrap()
-    }
-
-    fn keep_as(n: u8) -> bool {
-        return n.is_ascii_alphanumeric()
-            || n == b'*'
-            || n == b'-'
-            || n == b'.'
-            || n == b'_'
-            || n == b'\''
-            || n == b'~'
-            || n == b'!'
-            || n == b'('
-            || n == b')';
-    }
-
-    fn to_dec_ascii(n: u8) -> u8 {
-        match n {
-            0 => 48,
-            1 => 49,
-            2 => 50,
-            3 => 51,
-            4 => 52,
-            5 => 53,
-            6 => 54,
-            7 => 55,
-            8 => 56,
-            9 => 57,
-            10 => b'A',
-            11 => b'B',
-            12 => b'C',
-            13 => b'D',
-            14 => b'E',
-            15 => b'F',
-            _ => 127
-        }
-    }
-
-}
+pub use querystring::QueryParams;
 
 #[cfg(test)]
 mod tests {
     use super::querystring;
     use std::collections::HashMap;
-    use crate::querystring::QueryParamGet;
+    use super::querystring::{
+        QueryParams
+    };
 
     #[test]
     fn it_works() {
@@ -194,30 +43,41 @@ mod tests {
     }
 
     fn test_stringify() {
-        let enc1 = querystring::stringify(vec![
+        let enc1 = QueryParams::from(vec![
             ("params", "www. baidu. com/百度搜索"),
             ("encSecKey", "查询字=-)(*&^%$#@!~符串+~·！@￥%……%^%$:\"'*','-','.' and '_'"),
-        ]);
+        ]).stringify();
         let res1: String= String::from("params=www.%20baidu.%20com%2F%E7%99%BE%E5%BA%A6%E6%90%9C%E7%B4%A2&encSecKey=%E6%9F%A5%E8%AF%A2%E5%AD%97%3D-)(*%26%5E%25%24%23%40!~%E7%AC%A6%E4%B8%B2%2B~%C2%B7%EF%BC%81%40%EF%BF%A5%25%E2%80%A6%E2%80%A6%25%5E%25%24%3A%22'*'%2C'-'%2C'.'%20and%20'_'&");
         assert_eq!(enc1, res1);
+
+        let e = QueryParams::from(
+            vec![
+                ("params", "度"),
+                ("enc", "=-)(&#+~·@…%^$:*-_."),
+            ]
+        ).stringify();
+        println!("s={}", e);
     }
 
     fn test_parse() {
         let query_str = "params=www. baidu. com/百度搜索&encSecKey=查询字-=)(*^%$#@!~符串+~·！@￥%……%^%$:\"'*','-','.' and '_'&love=lu";
-        let res = querystring::parse(query_str);
+        let q = QueryParams::from(query_str);
+        let res1 = q.value("params").unwrap();
+        let res2 = q.value("encSecKey").unwrap();
+        let res3 = q.value("love").unwrap();
 
-        assert_eq!(res.get("params").unwrap(), &"www. baidu. com/百度搜索");
-        assert_eq!(res.get("encSecKey").unwrap(), &"查询字-=)(*^%$#@!~符串+~·！@￥%……%^%$:\"'*','-','.' and '_'");
-        assert_eq!(res.get("love").unwrap(), &"lu");
+        assert_eq!(res1, String::from("www. baidu. com/百度搜索"));
+        assert_eq!(res2, String::from("查询字-=)(*^%$#@!~符串+~·！@￥%……%^%$:\"'*','-','.' and '_'"));
+        assert_eq!(res3, String::from("lu"));
     }
 
 
     fn test_json() {
         let query_str1 = "idx=1024&name=lumi&family=ti=an&love";
-        let res1 = querystring::json(query_str1);
+        let res1 = QueryParams::from(query_str1).json();
 
         let query_str2 = "encSecKey=查询字=-)(*&^%$#@!~符串+~·！@￥%……%^%$:\"'*','-','.' and '_'";
-        let res2 = querystring::json(query_str2);
+        let res2 = QueryParams::from(query_str2).json();
 
         assert_eq!(res1, r#"{"idx":"1024","name":"lumi","family":"ti=an","love":""}"#);
 
@@ -225,12 +85,12 @@ mod tests {
     }
 
     fn test_query_get_traits() {
-        let query_str1 = "idx=1024&name=lumin&family=ti=an&love&ti=liang";
-        let name = query_str1.get_value("name").unwrap();
-        let idx = query_str1.get_value("idx").unwrap();
-        let family = query_str1.get_value("family").unwrap();
-        let love = query_str1.get_value("love").unwrap();
-        let no = query_str1.get_value("error").unwrap_or("error");
+        let query_params = QueryParams::from("idx=1024&name=lumin&family=ti=an&love&ti=liang");
+        let name = query_params.value("name").unwrap();
+        let idx = query_params.value("idx").unwrap();
+        let family = query_params.value("family").unwrap();
+        let love = query_params.value("love").unwrap();
+        let no = query_params.value("error").unwrap_or("error");
 
         assert_eq!(idx, "1024");
         assert_eq!(name, "lumin");
@@ -238,23 +98,12 @@ mod tests {
         assert_eq!(love, "");
         assert_eq!(no, "error");
 
-        let query2 = query_str1.replace_key("ti", "tian");
-        let query3 = query_str1.replace_key("love", "ai");
-        let query4 = query_str1.replace_value("ti=an", "[ti=an]");
-        let query5 = query_str1.replace_value("liang", "[liang]");
+        let mut query_params = query_params;
 
-        assert_eq!(query2, "idx=1024&name=lumin&family=ti=an&love&tian=liang");
-        assert_eq!(query3, "idx=1024&name=lumin&family=ti=an&ai&ti=liang");
-        assert_eq!(query4, "idx=1024&name=lumin&family=[ti=an]&love&ti=liang");
-        assert_eq!(query5, "idx=1024&name=lumin&family=ti=an&love&ti=[liang]");
+        let query2 = query_params.replace_key("ti", "tian").stringify();
+        let query3 = query_params.replace_key("love", "ai").stringify();
 
-        let q1 = "uid=1024&type=1086&/update/desc=desc_update_value";
-        let r1 = q1.replace_keys(vec!["uid", "/update/desc"], vec!["userId", "update_desc"]);
-        let r2 = q1.replace_values(vec!["1086", "desc_update_value"], vec!["10086", "update_desc_value"]);
-
-        assert_eq!(r1, "userId=1024&type=1086&update_desc=desc_update_value");
-        assert_eq!(r2, "uid=1024&type=10086&/update/desc=update_desc_value")
-
-
+        assert_eq!(query2, String::from("idx=1024&name=lumin&family=ti%3Dan&love&tian=liang&"));
+        assert_eq!(query3, String::from("idx=1024&name=lumin&family=ti%3Dan&ai&ti=liang&"));
     }
 }
---
+++ src/querystring.rs
@@ -0,0 +1,203 @@
+//! querystring module include the struct QueryParams
+
+use std::slice::Iter;
+use std::ops::Deref;
+
+type QueryParam<'a> = (&'a str, &'a str);
+
+#[derive(Debug, Clone)]
+pub struct QueryParams<'a> {
+    inner: Vec<QueryParam<'a>>,
+}
+
+impl<'a> QueryParams<'a> {
+
+    ///! ```rust
+    /// use urlqstring::QueryParams;
+    /// fn main() {
+    ///     let vec = vec![("id","1024"),("name","rust")];
+    ///     let q = QueryParams::from(vec).stringify();
+    ///     assert_eq!(q, "id=1024&name=rust&");
+    /// }
+    /// ```
+    pub fn stringify(&self) -> String {
+        self.iter().fold(String::new(), |acc, x| {
+            if x.1.is_empty() {
+                acc + &self.escape(&x.0) + "&"
+            } else {
+                acc + &self.escape(&x.0) + "=" + self.escape(&x.1).deref() + "&"
+            }
+        })
+    }
+
+    ///!```rust
+    /// use urlqstring::QueryParams;
+    /// fn main() {
+    ///     let res = QueryParams::from(vec![("id","1024"), ("name","rust")]).json();
+    ///     assert_eq!(res, r#"{"id":"1024","name":"rust"}"#)
+    /// }
+    ///```
+    pub fn json(&self) -> String {
+        let mut res: String = String::new();
+        let len = self.inner.len();
+        let mut idx = 0;
+        for (k,v) in &self.inner {
+            res.push_str(&format!(r#""{}":"{}""#, k, v));
+            idx += 1;
+            if idx < len {
+                res.push_str(",")
+            }
+        }
+        format!("{{{}}}", res)
+    }
+
+    ///!```rust
+    /// use urlqstring::QueryParams;
+    /// fn main() {
+    ///     let res = QueryParams::from(vec![("id","1024"), ("name","rust")]).value("name");
+    ///     assert_eq!(res, "rust");
+    /// }
+    /// ```
+    pub fn value(&self, key: &str) -> Option<&str> {
+        for query in &self.inner {
+            if query.0 == key {
+                return Some(query.1);
+            }
+        }
+        None
+    }
+
+    ///!```rust
+    /// use urlqstring::QueryParams;
+    /// fn main() {
+    ///     let res = QueryParams::from(vec![("id","1024"), ("name","rust")])
+    ///         .replace_key("name", "language")
+    ///         .stringify();
+    ///     assert_eq!(res, "id=1024&language=rust&");
+    /// }
+    /// ```
+    pub fn replace_key<'b: 'a>(&mut self, old_key: &str, new_key: &'b str) -> Self {
+        let mut res: Vec<QueryParam<'a>> = Vec::new();
+        self.iter().map(|(k, v)| {
+            if *k == old_key {
+                Some((new_key, *v))
+            } else {
+                Some((*k, *v))
+            }
+        }).for_each(|vec|{
+            res.push(vec.unwrap())
+        });
+
+        QueryParams {
+            inner: res
+        }
+    }
+
+
+    pub fn iter(&self) -> Iter<QueryParam<'a>> {
+        self.inner.iter()
+    }
+
+    fn from_str(s: &'a str) -> Self {
+        let query_str = s.split('&').collect::<Vec<&str>>();
+        let mut result: Vec<QueryParam<'a>> = Vec::new();
+        for query in query_str {
+            let key_value = query.splitn(2, '=').collect::<Vec<&str>>();
+            if key_value.len() > 1 {
+                result.push(( key_value[0], key_value[1] ) );
+            } else {
+                result.push(( key_value[0], "" ) );
+            }
+        }
+        QueryParams {
+            inner: result
+        }
+    }
+
+    fn from_vec(vec: Vec<QueryParam<'a>>) -> Self {
+        QueryParams {
+            inner: vec
+        }
+    }
+
+    fn escape(&self, str: &str) -> String {
+        let mut enc = Vec::<u8>::new();
+        for ch in str.as_bytes() {
+            if self.keep_as(*ch) {
+                enc.push(*ch);
+            } else {
+                enc.push(0x25);
+                let n1 = (*ch >> 4) & 0xf;
+                let n2 = *ch & 0xf;
+                enc.push(self.to_dec_ascii(n1));
+                enc.push(self.to_dec_ascii(n2));
+            }
+        }
+        String::from_utf8(enc).unwrap()
+    }
+
+    fn keep_as(&self, n: u8) -> bool {
+        return n.is_ascii_alphanumeric()
+            || n == b'*'
+            || n == b'-'
+            || n == b'.'
+            || n == b'_'
+            || n == b'\''
+            || n == b'~'
+            || n == b'!'
+            || n == b'('
+            || n == b')';
+    }
+
+    fn to_dec_ascii(&self, n: u8) -> u8 {
+        match n {
+            0 => 48,
+            1 => 49,
+            2 => 50,
+            3 => 51,
+            4 => 52,
+            5 => 53,
+            6 => 54,
+            7 => 55,
+            8 => 56,
+            9 => 57,
+            10 => b'A',
+            11 => b'B',
+            12 => b'C',
+            13 => b'D',
+            14 => b'E',
+            15 => b'F',
+            _ => 127
+        }
+    }
+}
+
+
+impl<'a> From<&'a str> for QueryParams<'a> {
+    fn from(s: &'a str) -> Self {
+        QueryParams::from_str(s)
+    }
+}
+
+impl<'a> From<Vec<QueryParam<'a>>> for QueryParams<'a> {
+    fn from(vec: Vec<QueryParam<'a>>) -> Self {
+        QueryParams::from_vec(vec)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::querystring::{ QueryParams };
+
+    #[test]
+    fn it_works() {
+        let res1 = QueryParams::from(vec![("id","1024"), ("idx","1,3,5")])
+            .replace_key("idx", "ids")
+            .json();
+        assert_eq!(res1, r#"{"id":"1024","ids":"1,3,5"}"#);
+
+        let res2 = QueryParams::from(vec![("id","1024"), ("name","rust")]).stringify();
+        assert_eq!(res2, "id=1024&name=rust&");
+    }
+}
+
